d223566d342c49286eca2c9a9155b64080821307
rafiadpr <work.rafiadrianp@gmail.com>
2024-09-05 14:30:21 +0700

    Percobaan alur dibalik

---
+++ PercobaanUploadJava23.java
@@ -0,0 +1,9 @@
+/**
+ * PercobaanUploadJava23
+ */
+public class PercobaanUploadJava23 {
+
+    public static void main(String[] args) {
+        System.out.println("Percobaan alur dibalik (Membuat folder kemudian di push ke GitHub)");
+    }
+}
